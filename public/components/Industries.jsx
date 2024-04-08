cc98504aa968ba40de144a21be3a435a3d345bac
tarunmalik330 <maliktarun2005@gmail.com>
2024-04-08 15:39:44 +0530

    chnage in industries img

--- public/components/Industries.jsx
+++ public/components/Industries.jsx
@@ -6,7 +6,7 @@ import Slider from "react-slick";
 import React from "react";
 import PlanningImg from "../assets/images/png/PlanningImg.png";
 import Image from "next/image";
-import relativeImg from "../assets/images/png/relativeimg.png";
+import relativeimg from "../assets/images/png/relativeimg.png";
 
 export default function Industries() {
   var settings = {
